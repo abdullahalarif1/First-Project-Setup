6e39a70a02845e2c49a7e9a33d9cef8cfec1070e
abdullahalarif1 <abdullahrabzil6@gmail.com>
2023-11-18 14:13:08 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+// all connection needed apply here.
+import app from "./app";
+import config from "./app/config";
+
+
+
+
+// getting-started.js
+import mongoose from "mongoose"; // apply import syntax
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  }
+   catch (err) {
+    console.log(err);
+  }
+}
+main()
